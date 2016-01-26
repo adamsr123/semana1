f6693c98deb3f166de1c8fc002aff58de396f60d
Adam Segall Rawet <Adam@imac-de-dm.local>
2016-01-26 17:46:12 +0200

    lol

---
+++ folder/acciones.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for i in 0...100
+{
+    if i % 2 == 0
+    {
+        print("\(i) par")
+    }
+    else if i % 2 != 0
+    {
+        print("\(i) inpar")
+    }
+    
+    if i < 40 && i > 30
+    {
+        print("\(i) viva swift!")
+    }
+    if i % 5 == 0
+    {
+        print("\(i) bingo")
+    }
+}
